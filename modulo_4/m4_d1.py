d540cd304fc4476a873541b714c0fe7fd955521b
Emanuele Notaro <notaro.back@gmail.com>
2023-09-06 18:49:36 +0200

    aggiunto esercizio 1 mod 4

---
+++ modulo_4/m4_d1.py
@@ -0,0 +1,14 @@
+print("studenti",studenti)
+altri_studenti=3
+studenti_totali=studenti+altri_studenti
+print("studenti totali",studenti_totali)
+lista_num = [0, 1, 2, 3, 4, 5]
+print("lista =",lista_num)
+''' è anche possibile fare una lista e stamparla
+con range
+e per stampare bisogna richiamare il vettore specificando il tipo
+'''
+lista_range=range(0,6)
+print("lista con funzione range",list(lista_range))
+film_preferiti = ["la ricerca della felicità","Mrs Doughtfire","moulin rouge"]
+print("questi sono i miei fil preferiti",film_preferiti)
